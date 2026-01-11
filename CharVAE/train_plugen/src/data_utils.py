15676ba90e2353eec2bd5080a560a919635cb980
Kevin Lai <laikevin279@gmail.com>
2026-01-11 02:02:50 -0500

    image grid saving

--- CharVAE/train_plugen/src/data_utils.py
+++ CharVAE/train_plugen/src/data_utils.py
@@ -1,22 +1,10 @@
-import gin
 import torch
-from torchtext import data, datasets
-from SmilesPE.pretokenizer import atomwise_tokenizer
+import torchvision.utils as vutils
 
 
-@gin.configurable
-def get_dataset(data_path, features):
-    BOS_WORD = '<s>'
-    EOS_WORD = '</s>'
-    BLANK_WORD = "<blank>"
+def save_image_grid(img_tensor: torch.Tensor, out_path: str, nrow: int = 8):
+    """
+    img_tensor: [B, 3, 64, 64] in [-1, 1] (because Normalize/Tanh)
+    """
+    vutils.save_image(img_tensor.detach().cpu(), out_path, normalize=True, nrow=nrow)
 
-    SRC = data.Field(tokenize=atomwise_tokenizer, init_token=BOS_WORD,
-                     eos_token=EOS_WORD, pad_token=BLANK_WORD)
-
-    data_fields = [('smiles', SRC)] + \
-                  [(feature, data.Field(is_target=True, use_vocab=False, sequential=False, preprocessing=float, dtype=torch.float)) for feature in features]
-
-    dataset = data.TabularDataset(path=data_path, format='csv', fields=data_fields, skip_header=True)
-
-    SRC.build_vocab(dataset)
-    return dataset, SRC, BOS_WORD, EOS_WORD, BLANK_WORD
